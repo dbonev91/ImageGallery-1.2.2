fa5581a8928a7c671982fcf10dae6d81d9cb927b
Dimitar Bonev <dbonev91@users.noreply.github.com>
2014-11-27 23:35:36 +0200

    Update generalVariables.js

--- js/generalVariables.js
+++ js/generalVariables.js
@@ -3,12 +3,14 @@ var GeneralVariables = (function () {
     var JSONImageData,
         clickedSmallImage,
         currentImage,
+        IMAGES_DIRECTORY = 'uploads/',
         DESIGN_DIRECTORY = 'design/';
 
     return {
         JSONImageData: JSONImageData,
         clickedSmallImage: clickedSmallImage,
         DESIGN_DIRECTORY: DESIGN_DIRECTORY,
+        IMAGES_DIRECTORY: IMAGES_DIRECTORY,
         currentImage: currentImage
     }
 
